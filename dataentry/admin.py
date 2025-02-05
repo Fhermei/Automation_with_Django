93aad724fb36b64b4b15f8fa45522a97f57fe019
Oyewole Oluwafemi David <123145430+Fhermei@users.noreply.github.com>
2025-02-05 17:26:15 +0100

    Imported data from CSV

--- dataentry/admin.py
+++ dataentry/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    """
+    Customizing the Django admin interface for the Student model.
+    This configuration allows enhanced functionalities in the admin interface.
+    """
+
+    # Fields to display in the list view
+    list_display = ('roll_no', 'name', 'age')
+
+    # Add search functionality based on roll_no and name
+    search_fields = ['roll_no', 'name']
+
+    # Add filters for age and date_of_birth
+    list_filter = ['age']
+
+    ordering = ['name']
+
+admin.site.register(Student, StudentAdmin)
--- dataentry/models.py
+++ dataentry/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    """
+    The Student model represents a student with:
+    - roll_no: A unique identifier for each student (maximum length 10).
+    - name: The student's name (maximum length 20).
+    - age: The student's age (integer value).
+    """
+
+    roll_no = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+    # In this case, it returns the student's name.
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.customer_name
